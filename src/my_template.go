85e29f9b8d861ba5a2390b4f9575a5a54b35a0a5
katsuki-a <katsuki1229s@gmail.com>
2022-07-07 22:53:33 +0900

    abc086

---
+++ src/my_template.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
