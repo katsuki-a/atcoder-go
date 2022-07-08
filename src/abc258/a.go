defcb684a22130e216aa2530ba32dc26e09669c6
katsuki-a <katsuki1229s@gmail.com>
2022-07-09 00:12:21 +0900

    abc258

---
+++ src/abc258/a.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func main() {
+	k := nextInt()
+	h := k / 60
+	m := k % 60
+	fmt.Printf("%02d:%02d\n", h+21, m)
+}
